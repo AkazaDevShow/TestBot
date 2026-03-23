925b838dff55b476f3c376756c28b6e828e0f686
AkazaDev <130745198+AkazaDevShow@users.noreply.github.com>
2026-03-23 23:40:15 +0300

    Create index.js
    sc

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+    if (msg.content === '!ping') {
+        msg.reply('Pong! 🏓');
+    }
+});
+
+// Railway uses environment variables for security
+client.login(process.env.DISCORD_TOKEN);
